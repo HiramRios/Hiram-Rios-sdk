5def46c03390e10dd7b515f79e0f998f85b597f3
Hiram Rios <harios@miners.utep.edu>
2022-09-23 22:48:17 -0500

    files

---
+++ main.py
@@ -0,0 +1,25 @@
+
+from funcions import *
+
+print( get_book())
+print("------------------------------------------------------------------")
+string_number = "5cf5805fb53e011a64671582"
+print( get_book_id(string_number))
+print("------------------------------------------------------------------")
+print(get_book_chapter(string_number))
+print("------------------------------------------------------------------")
+print(get_movies())
+print("------------------------------------------------------------------")
+string_numbers = "5cd95395de30eff6ebccde5d"
+print( get_movie_id(string_numbers))
+print("------------------------------------------------------------------")
+print( get_movie_quote(string_numbers))
+print("------------------------------------------------------------------")
+print( get_character())
+print("------------------------------------------------------------------")
+string_numbersCh = "5cd99d4bde30eff6ebccfbda"
+print( get_character_id(string_numbersCh))
+print("------------------------------------------------------------------")
+print( get_quote())
+print("------------------------------------------------------------------")
+print( get_chapter())
